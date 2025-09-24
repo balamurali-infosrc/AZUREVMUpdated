4beede09130d8c0a708b94726c5f40475cec0bee
Balamurali Ramakrishnan <balamurali.ramakrishnan@infoservices.com>
2025-09-24 16:56:26 +0530

    AZVMstartstopyml

--- main.tf
+++ main.tf
@@ -14,35 +14,4 @@ tenant_id = "63b9a1c1-375c-42cf-9c63-dc3798c7ae5e"
 //client_id       = "YYYYYY-YYYY-YYYY-YYYY-YYYYYYYYY" # this is app_id
 //client_secret   = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" # password
 }
-resource "azurerm_automation_account" "automation" {
-  name                = "myAutomation"
-  location            = azurerm_resource_group.azuretest_rg.location
-  resource_group_name = azurerm_resource_group.azuretest_rg.name
-  sku_name            = "Basic"
-   # Enable system-assigned managed identity
-  identity {
-    type = "SystemAssigned"
-  }
-}
 
-resource "azurerm_role_assignment" "automation_contributor" {
-  principal_id   = azurerm_automation_account.automation.identity[0].principal_id
-  role_definition_name = "Contributor"
-  scope = azurerm_resource_group.azuretest_rg.id
-}
-
-resource "azurerm_automation_schedule" "daily_schedule" {
-  name                    = "DailyVMStopStart"
-  resource_group_name     = azurerm_resource_group.azuretest_rg.name
-  automation_account_name = azurerm_automation_account.automation.name
-  frequency               = "Day"
-  interval                = 1
-#   time_zone               = "India Standard Time"
-  start_time              = "2025-09-25T22:00:00Z"  # UTC time
-}
-resource "azurerm_automation_job_schedule" "vm_job_schedule" {
-  automation_account_name = azurerm_automation_account.automation.name
-  resource_group_name     = azurerm_resource_group.azuretest_rg.name
-  runbook_name            = azurerm_automation_runbook.vm_runbook.name
-  schedule_name           = azurerm_automation_schedule.daily_schedule.name
-}
